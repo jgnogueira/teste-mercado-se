3451ec46d9729eec4fd5061de9cbca652b3880b3
José Gabriel <josegmnogueira@hotmail.com>
2023-02-27 13:21:22 -0300

    Ajustes

---
+++ src/Helper/ConexaoHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+class ConexaoHelper
+{
+    private static ?PDO $conexao    = null;
+    private static string $host     = 'localhost';
+    private static string $usuario  = 'postgres';
+    private static string $senha    = 'rootlocaldb';
+    private static string $database = 'mercado';
+    private static int $port        = 5432;
+
+    private static function realizarConexao() : PDO
+    {
+        if (self::$conexao === null) {
+            self::$conexao = new PDO('pgsql:host=' . self::$host . ';port=' . self::$port . ';dbname=' . self::$database, self::$usuario, self::$senha);
+            self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+
+        return self::$conexao;
+    }
+
+    public static function retornarConexao() : PDO
+    {
+        return self::realizarConexao();
+    }
+}
