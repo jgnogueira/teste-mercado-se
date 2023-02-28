f5da474946e818a0879b276400995fcfa23e1d1c
José Gabriel <josegmnogueira@hotmail.com>
2023-02-27 23:03:02 -0300

    Ajustes

--- src/DAO/ImpostoTipoProdutoDAO.php
+++ src/DAO/ImpostoTipoProdutoDAO.php
@@ -7,7 +7,12 @@ class ImpostoTipoProdutoDAO
     public static function cadastrarImposto(int $percentual, int $tipoProduto) : void
     {
         $conexao = ConexaoHelper::retornarConexao();
-        $query   = $conexao->prepare("INSERT INTO tbimposto_tipos_produto (id_tipo, porcentagem_imposto) VALUES (:tipo, :percentual) ");
+
+        $query = $conexao->prepare("DELETE FROM tbimposto_tipos_produto WHERE id_tipo = :tipo ");
+
+        $query->execute([':tipo' => $tipoProduto]);
+
+        $query = $conexao->prepare("INSERT INTO tbimposto_tipos_produto (id_tipo, porcentagem_imposto) VALUES (:tipo, :percentual) ");
 
         $query->execute([':tipo' => $tipoProduto, ':percentual' => $percentual]);
     }
