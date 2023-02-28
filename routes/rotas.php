ec2ce867f5c4db6e5f1bcd5f651f87be66794908
José Gabriel <josegmnogueira@hotmail.com>
2023-02-27 21:20:28 -0300

    Adicionando novas rotas

--- routes/rotas.php
+++ routes/rotas.php
@@ -1,13 +1,17 @@
 <?php
 
 $rotas = [
-    '/login'                  => 'LoginController@exibirPagina@GET',
-    '/realizar-login'         => 'LoginController@realizarLogin@POST',
-    '/cadastro'               => 'CadastroController@exibirPagina@GET',
-    '/criar-conta'            => 'CadastroController@criarConta@POST',
-    '/lista-de-produtos'      => 'ListaDeProdutosController@exibirPagina@GET',
-    '/logout'                 => 'LogoutController@realizarLogout@GET@autenticado',
-    '/404'                    => 'NaoEncontradoController@exibirPagina@GET',
-    '/cadastrar-tipo-produto' => 'CadastrarTipoProdutoController@exibirPagina@GET@autenticado',
-    '/salvar-tipo-produto'    => 'CadastrarTipoProdutoController@cadastrarTipo@POST@autenticado',
+    '/login'                          => 'LoginController@exibirPagina@GET',
+    '/realizar-login'                 => 'LoginController@realizarLogin@POST',
+    '/cadastro'                       => 'CadastroController@exibirPagina@GET',
+    '/criar-conta'                    => 'CadastroController@criarConta@POST',
+    '/lista-de-produtos'              => 'ListaDeProdutosController@exibirPagina@GET',
+    '/logout'                         => 'LogoutController@realizarLogout@GET@autenticado',
+    '/404'                            => 'NaoEncontradoController@exibirPagina@GET',
+    '/cadastrar-tipo-produto'         => 'CadastrarTipoProdutoController@exibirPagina@GET@autenticado',
+    '/salvar-tipo-produto'            => 'CadastrarTipoProdutoController@cadastrarTipo@POST@autenticado',
+    '/cadastrar-imposto-tipo-produto' => 'CadastrarImpostoTipoProdutoController@exibirPagina@GET@autenticado',
+    '/salvar-imposto-tipo-produto'    => 'CadastrarImpostoTipoProdutoController@cadastrarImposto@POST@autenticado',
+    '/cadastrar-produto'              => 'CadastrarProdutoController@exibirPagina@GET@autenticado',
+    '/salvar-produto'                 => 'CadastrarProdutoController@cadastrarProduto@POST@autenticado',
 ];
