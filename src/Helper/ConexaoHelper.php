669db2785bfcc75f91b4c60dea0278dba1ce59fe
José Gabriel <josegmnogueira@hotmail.com>
2023-03-01 09:48:18 -0300

    Ajustes

--- src/Helper/ConexaoHelper.php
+++ src/Helper/ConexaoHelper.php
@@ -2,18 +2,21 @@
 
 class ConexaoHelper
 {
-    private static ?PDO $conexao    = null;
-    private static string $host     = 'localhost';
-    private static string $usuario  = 'postgres';
-    private static string $senha    = 'rootlocaldb';
-    //private static string $database = 'mercado';
-    private static string $database = 'test_mercado';
-    private static int $port        = 5432;
+    private static bool $ambienteTeste  = true;
+    private static ?PDO $conexao        = null;
+    private static string $host         = 'localhost';
+    private static string $usuario      = 'postgres';
+    private static string $senha        = 'rootlocaldb';
+    private static string $database     = 'mercado';
+    private static string $testDatabase = 'test_mercado';
+    private static int $port            = 5432;
 
     private static function realizarConexao() : PDO
     {
+        $database = self::$ambienteTeste ? self::$testDatabase : self::$database;
+
         if (self::$conexao === null) {
-            self::$conexao = new PDO('pgsql:host=' . self::$host . ';port=' . self::$port . ';dbname=' . self::$database, self::$usuario, self::$senha);
+            self::$conexao = new PDO('pgsql:host=' . self::$host . ';port=' . self::$port . ';dbname=' . $database, self::$usuario, self::$senha);
             self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         }
